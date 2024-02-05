94742310bf5ad96527373fc6770c7cea126d5192
John <jeobando11@hotmail.com>
2024-02-05 17:52:05 -0500

    conectar DB con Render online

--- backendMiMascota/libs/sequelize.js
+++ backendMiMascota/libs/sequelize.js
@@ -3,9 +3,16 @@ const { Sequelize } = require('sequelize');
 const { config } = require('./../config/config');
 const setupModels = require('./../db/models');
 
+// console.log(process.env.NODE_ENV)
+// console.log(config.isProd)
+// if (config.isProd == true)
+// {
+//   console.log("Estamos activoS!!")
+// }
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+// const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://mimascota:tr3qQ45bU8bs0jT34l2DyLEn6fvozWZ4@dpg-cn0m0vla73kc73ebvop0-a.oregon-postgres.render.com/mimascotadb`;
 
 const sequelize = new Sequelize(URI, {
   dialect: 'postgres',
@@ -17,7 +24,8 @@ setupModels(sequelize); //Recibe la conexion
 //Crear sincronizacion
 // Evitar sincronización automática en producción
 if (process.env.NODE_ENV !== 'production') {
-  sequelize.sync()
+  sequelize
+    .sync()
     .then(() => {
       console.log('Sincronización de modelos completa.');
     })
